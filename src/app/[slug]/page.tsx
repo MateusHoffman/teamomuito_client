8efe90b73b856a83b8c75bb05b961518988b990a
Hoffman <mateushoffmandev@gmail.com>
2024-10-03 22:01:31 -0300

    fullscreen

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+"use client"
 import { useEffect, useState } from "react";
 import Preview from "../components/preview/Preview";
 import "@/app/assets/styles/scrollbar.css";
@@ -38,6 +37,23 @@ const Result = ({ params }: { params: { slug: string } }) => {
     })();
   }, [params?.slug]);
 
+  const goFullscreen = () => {
+    const elem: any = document.documentElement;
+
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      // Firefox
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      // Chrome, Safari e Opera
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      // IE/Edge
+      elem.msRequestFullscreen();
+    }
+  };
+
   if (openPresent === false) {
     return (
       <div className="flex flex-col justify-center items-center h-screen bg-[#030d21] px-4 max-w-screen w-screen">
@@ -47,11 +63,14 @@ const Result = ({ params }: { params: { slug: string } }) => {
             alt="Logo Te Amo Muito"
             fill
             style={{ objectFit: "contain" }}
-            priority // Add this prop
+            priority
           />
         </div>
         <button
-          onClick={() => setOpenPresent(true)}
+          onClick={() => {
+            setOpenPresent(true);
+            goFullscreen();
+          }}
           className="p-4 border-2 border-[#DD443A] rounded-lg mt-4"
         >
           <span className="font-bold tracking-wide text-center text-white uppercase">
@@ -78,7 +97,7 @@ const Result = ({ params }: { params: { slug: string } }) => {
     );
   }
 
-  if (data?.paymentStatus !== 'approved') {
+  if (data?.paymentStatus !== "approved") {
     return (
       <div className="flex items-center justify-center h-screen bg-[#030d21] px-4">
         <h1 className="text-white">Pagamento não foi concluído</h1>
